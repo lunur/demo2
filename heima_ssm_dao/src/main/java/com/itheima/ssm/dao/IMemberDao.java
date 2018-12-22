19247090e342170afa603d21310228c0ad49af49
JSLY <zh_f_l@yeah.net>
2018-12-22 17:25:11 +0800

    xiugai

--- heima_ssm_dao/src/main/java/com/itheima/ssm/dao/IMemberDao.java
+++ heima_ssm_dao/src/main/java/com/itheima/ssm/dao/IMemberDao.java
@@ -6,6 +6,5 @@ import org.apache.ibatis.annotations.Select;
 public interface IMemberDao {
 
     @Select("select * from member where id=#{id}")
-
     public Member findById(String id) throws Exception;
 }
